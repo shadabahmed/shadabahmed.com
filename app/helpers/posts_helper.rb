dff585f83fa7fc5a5b21a8487590198faee1e74b
shadabahmed <shadab.ansari@gmail.com>
2012-02-22 15:49:22 +0530

    Added facebook like button to posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,4 +4,11 @@ module PostsHelper
   def more_content?
     @posts.size == Post::DEFAULT_LIMIT
   end
+  
+  def facebook_like(url)
+    puts '*****************'
+    puts url
+    content_tag :iframe, nil, :src => "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(url)}&layout=standard&show_faces=true&width=450&action=like&font=arial&colorscheme=light&height=80", :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like
+  end
+  
 end
